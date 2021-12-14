d9f830d63c0efaf41854b658c0fd707dca1fef2c
Artur Debski <xmentero@gmail.com>
2021-12-14 16:19:24 +0100

    Relax and bump dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+# v0.4.0
+
+* Relaxed and updated dependencies
+* Minimum Elixir version is now 1.4
+
 # v0.3.3
 
 * Fix file extension detection
--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
 defmodule Bow.Mixfile do
   use Mix.Project
 
-  @version "0.3.3"
+  @version "0.4.0"
 
   def project do
     [
       app: :bow,
       version: @version,
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -24,13 +24,8 @@ defmodule Bow.Mixfile do
 
   def application do
     [
-      applications: [:logger, :plug, :tesla] ++ applications(Mix.env())
+      extra_applications: [:logger] ++ applications(Mix.env())
     ]
-
-    # TODO: Uncomment wneh dropping support for elixir 1.3
-    # [
-    #   extra_applications: [:logger] ++ applications(Mix.env)
-    # ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -57,17 +52,17 @@ defmodule Bow.Mixfile do
       {:tesla, "~> 1.0"},
       {:ecto, "~> 3.2", optional: true},
       {:ecto_sql, "~> 3.2", optional: true},
-      {:erlexec, "~> 1.9.0", optional: true},
+      {:erlexec, "~> 1.19", optional: true},
       {:ex_aws, "~> 2.0", optional: true},
       {:ex_aws_s3, "~> 2.0", optional: true},
-      {:sweet_xml, "~> 0.6.5", optional: true},
+      {:sweet_xml, "~> 0.7", optional: true},
 
       # testing & docs
       {:postgrex, ">= 0.0.0", only: :test},
-      {:coverex, "~> 1.4.10", only: :test},
+      {:coverex, "~> 1.5", only: :test},
       {:ex_doc, "~> 0.21", only: :dev},
-      {:mix_test_watch, "~> 0.5.0", only: :dev},
-      {:dialyxir, "~> 1.0", only: :dev}
+      {:mix_test_watch, "~> 1.1", only: :dev},
+      {:dialyxir, "~> 1.1", only: :dev}
     ]
   end
 
